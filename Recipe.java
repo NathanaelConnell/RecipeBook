b5facadebb3d22875030c6f9a0d5fe4701453a31
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-03-11 12:01:17 -0600

    Update Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -1,3 +1,19 @@
-class Recipe {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
+class Recipe {
+    private String title; "NO TITLE";
+    private String description; "NO DESCRIPTION";
+    private BufferedImage image;
+    private ArrayList<Ingredient> ingredients;
+    private ArrayList<String> instructions;
+
+    public Recipe(title = "NO TITLE", description = "NO DESCRIPTION"; image = new BufferedImage(), ingredients = new ArrayList<>, instructions = new ArrayList<>) {
+        
+    }
 }
