21fbd43515b0fb7e598925099a8c641e90b6872f
aPannell064 <adamkpannell@gmail.com>
2025-03-20 10:06:10 -0600

    Import Images

--- Recipe.java
+++ Recipe.java
@@ -2,18 +2,45 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 class Recipe {
-    private String title; "NO TITLE";
-    private String description; "NO DESCRIPTION";
+    private String title;
+    private String description;
     private BufferedImage image;
     private ArrayList<Ingredient> ingredients;
     private ArrayList<String> instructions;
 
-    public Recipe(title = "NO TITLE", description = "NO DESCRIPTION"; image = new BufferedImage(), ingredients = new ArrayList<>, instructions = new ArrayList<>) {
-        
+    public Recipe (String title, String description, BufferedImage image, 
+    ArrayList<Ingredient> ingredients, ArrayList<String> instructions) {
+        //Constructor
+    }
+    
+
+    public void importImage() {
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files (JPG, PNG, GIF)", "jpg", "jpeg", "png", "gif");
+        fileChooser.setFileFilter(filter);
+
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            try {
+                BufferedImage selectedImage = ImageIO.read(selectedFile);
+                if (selectedImage != null) {
+                    int cropLength = Math.min(selectedImage.getWidth(), selectedImage.getHeight());
+                    image = selectedImage.getSubimage(Math.max((selectedImage.getWidth()-cropLength)/2, 0),
+                            Math.max((selectedImage.getHeight()-cropLength)/2, 0),
+                            cropLength, cropLength);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid image format.");
+                }
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Error uploading image: " + e.getMessage());
+            }
+        }
     }
 }
