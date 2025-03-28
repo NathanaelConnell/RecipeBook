53a02de4352465a2971ad03ad5407f6a3f35e562
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-03-28 15:20:03 -0600

    Update Ingredient.java

--- Ingredient.java
+++ Ingredient.java
@@ -1,5 +1,11 @@
 class Ingredient {
-    String units;
     String name;
-    float amount; //Datatype may change
+    public Ingredient(String title) {
+        this.setName(title);
+    }
+
+    public String getName() {return name;}
+
+    public void setName(newName) {name = newName;}
+    
 }
