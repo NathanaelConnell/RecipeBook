c85e229d6c37a81bdf5c82da84b487df95f94039
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-04-14 23:39:35 -0600

    Update RecipeRepo.java

--- RecipeRepo.java
+++ RecipeRepo.java
@@ -1,1 +1,19 @@
+//import com.github.recipeDBCode;
 
+class RecipeRepo {
+    
+    //Make sure you can connect into database
+    private static void connect() {
+        
+    }
+    
+    //Save recipe data to database
+    private static void save () {
+        
+    }
+    
+    //Load recipe data to database
+    private static void load() {
+        
+    }
+}
