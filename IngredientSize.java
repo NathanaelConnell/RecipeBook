e57d0475d03f31770c04394d49f868438d555c6d
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-05-01 01:01:44 -0600

    Update IngredientSize.java

--- IngredientSize.java
+++ IngredientSize.java
@@ -8,4 +8,12 @@ class IngredientSize {
     }
 
     public String toString() {return amount + " " + unit;}
+
+    public static IngredientSize fromString(String amount) {
+        String[] fractionElements = amount.trim().split("\\s+", 2);
+        Fraction fractionAmount = Fraction.fromString(fractionElements[0]);
+        String unit = fractionElements.length > 1 ? fractionElements[1] : "";
+
+        return new IngredientSize(fractionAmount, unit);
+    }
 }
